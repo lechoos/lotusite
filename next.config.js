0cd071a40e6af8ffc549d615817599e18fbf338b
Piotr Lechowicz <ptrlechowicz@gmail.com>
2024-03-24 00:22:16 +0100

    1.0.2

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["192.109.244.63", "localhost:3000"]
+    }
+  }
 }
