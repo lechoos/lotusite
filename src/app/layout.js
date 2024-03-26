c66d0bcc2d407b2e5b7f1ed16ddb50bf866e4463
Piotr Lechowicz <ptrlechowicz@gmail.com>
2024-03-26 01:20:00 +0100

    1.0.6

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Open_Sans } from 'next/font/google';
+import { Head } from 'next/document';
 import './globals.scss';
 import { Footer } from '@/components/footer/Footer';
 
@@ -15,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='pl-PL'>
+			<Head>
+				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+			</Head>
 			<body className={openSans.className}>
 				{children}
 				<Footer />
