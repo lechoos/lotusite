d044df7bfc3a3c078af13f8d87f36220307c8659
Piotr Lechowicz <ptrlechowicz@gmail.com>
2024-04-02 13:34:12 +0200

    2.0.1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Open_Sans } from 'next/font/google';
 import './globals.scss';
+import { AOS } from './AOS';
 import { Footer } from '@/components/footer/Footer';
 
 const openSans = Open_Sans({
@@ -18,12 +19,16 @@ export const metadata = {
       },
     ],
   },
+	robots: {
+		noimageindex: true
+	}
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang='pl-PL'>
 			<body className={openSans.className}>
+				<AOS />
 				{children}
 				<Footer />
 			</body>
